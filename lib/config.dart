5b1ac217077d29dad768310c380ed6aeb2fac4b1
Andria <Andria_123@gmail.com>
2025-06-22 13:07:39 +0300

    Initial commit

---
+++ lib/config.dart
@@ -0,0 +1,4 @@
+class SiteConfig {
+  static const String siteName = 'My Website';
+  static const String apiUrl = 'https://example.com/api';
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'config.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: SiteConfig.siteName,
+      home: Scaffold(
+        appBar: AppBar(title: Text(SiteConfig.siteName)),
+        body: Center(child: Text('Connected to: ${SiteConfig.apiUrl}')),
+      ),
+    );
+  }
+}
